c6dbd6944816836fbf2ebc36d552f38abe10c640
ashpydev <ash.pydev@gmail.com>
2025-02-27 19:21:46 -0600

    .

--- Scripts/scriptTesting/script_Groovy_demo/Script1740087025224.groovy
+++ Scripts/scriptTesting/script_Groovy_demo/Script1740087025224.groovy
@@ -4,16 +4,16 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+WebUI.navigateToUrl(GlobalVariable.url)
 
 WebUI.waitForPageLoad(10)
 
 // println "Global Variable Value: " + GlobalVariable.username
 // println "Global Variable Value: " + GlobalVariable.password
 
-WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input_Username_username'), "admin")
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/input_Username_username'), GlobalVariable.username)
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input_Password_password'), "hUKwJTbofgPU9eVlw/CnDQ==")
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_OrangeHRM/input_Password_password'), GlobalVariable.password)
 
 WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/button_Login'))
 
