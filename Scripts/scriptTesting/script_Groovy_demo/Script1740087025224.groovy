c21e05525b7f36df02d966852fb514abc87ba31e
ashpydev <ash.pydev@gmail.com>
2025-02-27 19:19:12 -0600

    .

--- Scripts/scriptTesting/script_Groovy_demo/Script1740087025224.groovy
+++ Scripts/scriptTesting/script_Groovy_demo/Script1740087025224.groovy
@@ -1,6 +1,6 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-//import internal.GlobalVariable as GlobalVariable
+import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('')
 
